f7aa0366edaa95adf398cd15f5b65636857393b4
Maialen <mdlplaza@gmail.com>
2020-10-19 21:08:41 +0200

    Completado

--- search.js
+++ search.js
@@ -2,13 +2,12 @@
 
 let filmsContent = document.getElementById("films");
 
-searchMovies(null, true);
+searchMovies(null);
 
-function searchMovies(event, discover) {
-    discover = discover || false;
-    let url = "";
-    if (!discover) {
-        let valueToSearch = event.target.value;
+function searchMovies(event) {
+    let valueToSearch = event ? event.target.value : "";
+
+    if (valueToSearch) {
         url = "https://api.themoviedb.org/3/search/movie?api_key=8c98db99e092e8bc99a6bb341fad6201&language=en-US&page=1&include_adult=false&query=" + valueToSearch;
     } else {
         url = "https://api.themoviedb.org/3/discover/movie?api_key=8c98db99e092e8bc99a6bb341fad6201&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
